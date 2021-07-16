728a1460ec82e464bcbc4844ac161d25e5e59dc2
GizemDayioglu <GizemDayioglu@hotmail.com>
2021-07-16 06:36:24 +0300

    Initial Commit

---
+++ mvvmsampleapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvmsampleapp
+//
+//  Created by Gizem Gulsen on 7/16/21.
+//  Copyright © 2021 Gizem Dayioglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
